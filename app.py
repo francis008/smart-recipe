fb62ec73390e255f7663ba56f5687875f5c5ca64
Francis <francischanziheng@gmail.com>
2025-04-16 23:44:28 -0700

    added input

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+
+    ingredients_input = None
+
+    if request.method == "POST":
+        ingredients_input = request.form.get('ingredients')
+        print(f"Received ingredients {ingredients_input}")
+        
+    return render_template('index.html', ingredients_received=ingredients_input) 
+    
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
